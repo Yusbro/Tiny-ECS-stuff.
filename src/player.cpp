46f410f0e39b51998d70aaa96525c3b49a80409c
Ram <100201070+Yusbro@users.noreply.github.com>
2023-06-16 06:44:34 +0000

    finished some stuff

--- src/player.cpp
+++ src/player.cpp
@@ -1,6 +1,9 @@
 #include "../include/player.hpp"
 #include "../include/components.hpp"
 
+#include <raymath.h>
+#include <iostream>
+
 void Player::init(World& world)
 {	
 	Position position = {10,10,10};
@@ -23,23 +26,30 @@ void Player::update(World& world)
 {
 	std::vector<int> player = world.Get_Archtype<Position, Camera, PlayerTag>();
 	
-	std::vector<Position> position = *world.Fetch_Data<Position>(player[0]);
-	std::vector<Camera> camera = *world.Fetch_Data<Camera>(player[0]);
+	std::vector<Position>* position = world.Fetch_Data<Position>(player[0]);
+	std::vector<Camera>* camera = world.Fetch_Data<Camera>(player[0]);
 	
-	Player::player_move(position[0], camera[0]);
+	Player::player_move((*position)[0], (*camera)[0]);
 }
 
 //private functions!!!
 inline void Player::player_move(Position& position, Camera& camera){
 	//getting da controls!
-	int x=0, y=0;
-	if(IsKeyDown(KEY_W)) y = 1;
+	float x=0, y=0;
+	if(IsKeyDown(KEY_W)) y = -1;
 	if(IsKeyDown(KEY_A)) x = -1;
-	if(IsKeyDown(KEY_S)) y = -1;
+	if(IsKeyDown(KEY_S)) y = 1;
 	if(IsKeyDown(KEY_D)) x = 1;
-
-	position.x += x;
-	position.z += y;
+		
+	
+	float move_angle = std::atan2(x, y) + 0.7853;
+	
+	std::cout<<sin(move_angle)<<"	"<<cos(move_angle)<<std::endl;
+	
+	if((x+y)!=0){
+		position.x += sin(move_angle) * 2;
+		position.z += cos(move_angle) * 2;
+	}
 
 	camera.position = position;
 	camera.target = {position.x - 10, position.y - 10, position.z - 10};
